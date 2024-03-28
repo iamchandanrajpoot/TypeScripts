f2ad5de6cc4d5e283816056ebc53ec302db73bbf
iamchandanrajpoot <iamchandanrajpoot@gmail.com>
2024-03-28 10:19:16 +0530

    working witih union types

--- app.ts
+++ app.ts
@@ -1,15 +1,23 @@
 const num1Element = document.getElementById("num1") as HTMLInputElement;
 const num2Element = document.getElementById("num2") as HTMLInputElement;
-const buttonElement = document.querySelector('button') as HTMLButtonElement;
+const buttonElement = document.querySelector('button')!;
 
-function add(num1: number, num2: number): number {
-  return num1 + num2;
+function add(num1: number |string, num2: number| string) {
+  if(typeof num1 === "number" && typeof num2 === "number"){
+    return num1 + num1;
+  }else if(typeof num1 === "string" && typeof num2 === "string"){
+    return num1 + " " + num2;
+  }else{
+    return +num1 + +num2;
+  }
 }
 
 buttonElement.addEventListener('click', () => {
-  const num1 = +num1Element.value; // Convert to number
-  const num2 = +num2Element.value; // Convert to number
+  const num1 = num1Element.value;
+  const num2 = num2Element.value; 
 
-  const result = add(num1, num2);
+  const result = add(+num1, +num2);
+  const stringResult = add(num1, num2);
   console.log(result);
+  console.log(stringResult);
 });
